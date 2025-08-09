87c26ed187030df69e26eda1a01676ce0276101e
Divyansh <divyanshsinghnrj@gmail.com>
2025-08-09 20:15:23 +0530

    201509082025

--- frontend.py
+++ frontend.py
@@ -7,11 +7,16 @@ from tensorflow.keras.preprocessing import image as keras_image
 
 st.set_page_config(page_title="Chest X-Ray Classifier", layout="centered")
 st.title("Chest X-Ray Classifier")
+# Load trained model
 @st.cache_resource
 def load_trained_model():
     return load_model("model.h5",compile=False)
+
 model = load_trained_model()
 class_names = ['COVID19', 'NORMAL', 'PNEUMONIA']
+
+
+
 st.write("Upload your own chest X-ray or select one of our sample images to get a prediction.")
 sample_dir = "./sample_images"  # put 3 images here
 sample_files = {
@@ -19,6 +24,7 @@ sample_files = {
     "Sample Img 2": "two.jpg",
     "Sample Img 3": "three.jpg"
 }
+
 option = st.selectbox("Or choose a sample X-ray:", ["None"] + list(sample_files.keys()))
 uploaded_file = st.file_uploader("Upload your chest X-ray (jpg/png)", type=["jpg", "png", "jpeg"])
 image_to_predict = None
@@ -29,16 +35,19 @@ if uploaded_file is not None:
 elif option != "None":
     image_to_predict = Image.open(os.path.join(sample_dir, sample_files[option])).convert("RGB")
     label = option
+
+# --- Prediction ---
 if image_to_predict:
     st.image(image_to_predict.resize((300, 300)), caption=label)
     img = image_to_predict.resize((224, 224))
     img_array = keras_image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
+
     prediction = model.predict(img_array)
     pred_index = np.argmax(prediction)
     predicted_label = class_names[pred_index]
     confidence = prediction[0][pred_index] * 100
 
-st.success(f"**Prediction:** {predicted_label} ({confidence:.1f}%)")
+    st.success(f"**Prediction:** {predicted_label} ({confidence:.1f}%)")
 st.write("For More Sample Image Use Kaggle Dataset")
 st.write("https://www.kaggle.com/datasets/paultimothymooney/chest-xray-pneumonia")
